927a48454d288adab4966c166e44c2c37096272d
Sneaky Koder <sneaky.koder>
2020-09-25 16:58:14 -0700

    Dockeer purge bash script update

---
+++ docker_purge.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -a -q)
+docker volume prune -f
+docker network rm $(docker network ls -q)
+
