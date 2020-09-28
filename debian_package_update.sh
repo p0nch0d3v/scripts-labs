85f1383db02b747dd7f0662d8ae4edc124390581
sneakykoder <sneaky.koder@gmail.com>
2020-09-28 09:24:18 -0700

    Debian update

--- debian_package_update.sh
+++ debian_package_update.sh
@@ -2,7 +2,7 @@
 
 clear
 sudo apt-get update -y
-sudo apt-get upgrade -y;
-sudo apt-get autoremove -y;
-sudo apt-get autoclean -y;
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
 
