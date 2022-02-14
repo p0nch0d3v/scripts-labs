f301149ea255c346c3f873663ef25e4b5229e653
Sneaky Koder <sneaky.koder@gmail.com>
2022-02-14 11:57:23 -0700

    Command updates

--- docker_clean_up.sh
+++ docker_clean_up.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-docker rm $(docker ps -a -q -f="status=exited")
-docker rm $(docker ps -a -q -f="status=dead")
-docker rmi $(docker images -a -q -f="dangling=true")
+docker container rm $(docker container ls -a -q -f="status=exited")
+docker container rm $(docker container ls -a -q -f="status=dead")
+docker image rm $(docker image ls -a -q -f="dangling=true")
+docker image prune --force
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q)
 
-docker ps -a
-docker images -a
+docker container ls -a
+docker image ls -a
 docker volume ls
 docker network ls
